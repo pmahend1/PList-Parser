9664582dc109a315f46ed3b3b1a09a7a4255dd73
pmahend1 <prateek.mahendrakar@gmail.com>
2021-10-16 21:52:25 -0400

    moved powershell project

---
+++ Powershell/PListParserLibrary.Powershell/PListParserLibrary.Powershell/PListParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace PListParserLibrary.Powershell
+{
+    [Cmdlet(VerbsCommon.Get, "PlistFromText")]
+    [OutputType(typeof(Dictionary<string, object>))]
+    public class PListParser : PSCmdlet
+    {
+        [Parameter(Position = 0, ValueFromPipelineByPropertyName = true)]
+        public string Input { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (Input is not null)
+            {
+                var plistParserLibrary = new Parser();
+                var output = plistParserLibrary.Parse(Input);
+                WriteObject(output);
+            }
+        }
+    }
+}
+
